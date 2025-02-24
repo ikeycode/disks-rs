b5e1ebfa96bd8cdc66b0317c40ee9c6164f3c4c0
Ikey Doherty <ikey@aerynos.com>
2025-02-24 23:52:39 +0000

    provisioning: Require a disk to create partition tables on
    Signed-off-by: Ikey Doherty <ikey@aerynos.com>

--- crates/provisioning/src/commands/create_partition_table.rs
+++ crates/provisioning/src/commands/create_partition_table.rs
@@ -2,20 +2,25 @@
 //
 // SPDX-License-Identifier: MPL-2.0
 
-use crate::Context;
 use crate::{get_kdl_property, FromKdlProperty, PartitionTableType};
+use crate::{get_property_str, Context};
 
 /// Command to create a partition table
 #[derive(Debug)]
 pub struct Command {
     /// The type of partition table to create
     pub table_type: PartitionTableType,
+    pub disk: String,
 }
 
 /// Generate a command to create a partition table
 pub(crate) fn parse(context: Context<'_>) -> Result<super::Command, crate::Error> {
     let kind = get_kdl_property(context.node, "type")?;
     let table_type = PartitionTableType::from_kdl_property(kind)?;
+    let disk = get_property_str(context.node, "disk")?;
 
-    Ok(super::Command::CreatePartitionTable(Box::new(Command { table_type })))
+    Ok(super::Command::CreatePartitionTable(Box::new(Command {
+        table_type,
+        disk,
+    })))
 }
